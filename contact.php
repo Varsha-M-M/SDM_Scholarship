2e1f3f027b63defda253041a2e607c04b4b6a210
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-13 22:07:03 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,15 +5,22 @@
         <meta charset="UTF-8">
         <title> Contact us Information</title>
         <script src="https://kit.fontawesome.com/e87ad9b826.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="contactus.css">
+        <link rel="stylesheet" href="registration.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div class="btn">
-            <a href="input1.php">Home</a>
-            <a href="about.php">About</a>
-            
-        </div>
+        <header>
+        <p class="headding">Contact Us</p>
+        <nav>
+            <ul class="nav-links">
+               <li><a href="input1.php">Home</a></li>
+               <li><a href="index1.php">SignUp</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="about.php">About-us</a></li>
+                
+            </ul>
+        </nav>
+    </header>
         <div class="box">
         <div class="container">
             <div class="content">
@@ -43,3 +50,81 @@
     </body>
     
 </html>
+
+<style>
+
+body{
+    
+    background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url("https://cache.careers360.mobi/media/presets/720X480/colleges/social-media/media-gallery/3332/2018/8/9/SDM-College-of-Engineering-and-Technology-Dharwad-7.jpg");
+    background-size: cover;
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    
+}
+.box{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    box-sizing: inherit;
+    min-width: 30%;
+    
+}
+.btn{
+    float: right;
+    margin-right: 10px;
+    padding: 10px;
+   
+ }
+ a{
+     color: #fff;
+     font: 1em sans-serif;
+     padding: 10px;
+     
+ }
+.container{
+   
+    
+    background: #fff;
+    border-radius: 6px;
+    padding: 20px 60px 40px 40px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+    align-items: center;
+     
+     
+}
+.content {
+    
+    align-items: center;
+}
+
+.content .left-side .details{
+    margin: 14px;
+    text-align: center;
+}
+.content .left-side .details i{
+    font-size: 30px;
+    color: #a6af13;
+    margin-bottom: 10px;
+
+}
+.content .left-side .details .text-one,
+.content .left-side .details .text-two{
+    font-size: 14px;
+    color: #868686;
+    
+}
+input[type],select{
+    min-width: 2%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 15px;
+    background-color: rgba(231, 225, 225, 0.658);
+    }
+</style>
