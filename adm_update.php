4f21b555698b7a95312a5fe087055efa1fc7b770
padmashri5 <75256386+padmashri5@users.noreply.github.com>
2022-06-13 21:40:43 +0530

    Update adm_update.php

--- adm_update.php
+++ adm_update.php
@@ -5,19 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>adm_update</title>
     <link href="registration.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
-    <div class="headline"><h1>Update SCHOLARSHIP
-        <div class="btn">
-          <input type="button" value="HOME"  >
-          <input type="button" value="about">
-           <input type="button" value="contact us">
-        </div>   </h1>  
-    </div> <br><br><br><br><br><br>
-    <div id="box">
+<header>
+        <p class="headding">Update SCHOLARSHIP</p>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="input1.php">Home</a></li>
+                
+                <li><a href="about.php">About-us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="formbody">
     <label id="l1">Choose a Scholarship:</label>
     <select name="scholar" id="scholar">
         <option value="fee_concession_for_disabels">Fee Concession for Disabels</option>
@@ -33,11 +37,13 @@
         <option value="sdme_trust">SDME Trust</option>
         <option value="skdrdp">SKDRDP</option>
         <option value="top_class_scheme_sc">Top Class Scheme SC</option>
-     </select><br><br>
-    <label> New Eligibility Criteria :</label>
-    <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
+     </select><br>
+    <label> New Eligibility Criteria :</label><br>
+    <textarea id="w3review" name="w3review" rows="4" cols="40"></textarea>
     <input type="submit" value="Notify Developers" id="btn">
     </div>
+    
+   
     
     
 </body>
