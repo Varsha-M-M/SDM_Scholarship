502216de08e3f4a5b329a5cdf42d50f0646df310
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-13 22:02:13 +0530

    Update input1.php

--- input1.php
+++ input1.php
@@ -4,28 +4,58 @@
     <head>
         <meta charset="UTF-8">
         <title>Website for SDM Scholarship</title>
-        <link rel="stylesheet" href="style1.css">
+        <link rel="stylesheet" href="registration.css">
     </head>
     <body>
         <header>
-            <div class="wrapper">
-                <div class ="logo">
-                    <img src="https://storage.googleapis.com/ezap-prod/colleges/3688/sdm-college-of-engineering-and-technology-dharwad-logo.jfif" alt="">
-                </div>
-                <ul class="nav-area">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="login.php">Login</a></li>
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="admin_login.php">Admin</a></li>
-                </ul>
-            </div>
-
+        <p class="headding" color:black>.</p>
+        <nav>
+            <ul class="nav-links">
+               
+                <li><a href="login.php">Login</a></li>
+                <li><a href="about.php">About-us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#">Query</a></li>
+                <li><a href="admin_login.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
             <div class="Welcome-text">
                 <h1>SDM scholarship </h1>
                 <a href="index1.php">Sign Up</a>
             </div>
-        </header>
+        
         
     </body>
 </html>
+
+<style>
+    .Welcome-text{
+    position: absolute;
+    width: 600px;
+    height: 300px;
+    margin: 20% 30%;
+    text-align: center;
+}
+.Welcome-text h1{
+    /* position: relative; */
+    /* text-align: center; */
+    color:#fff;
+    text-transform: uppercase;
+    font-size: 50px;
+}
+.Welcome-text a{
+    border: 1px solid #fff;
+    padding: 10px 25px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 14px;
+    margin-top: 20px;
+    display: inline-block;
+    color:#fff;
+}
+.Welcome-text a:hover{
+    background:#fff;
+    color: #333;
+}
+</style>
