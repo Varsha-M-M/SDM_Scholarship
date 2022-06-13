672674fd8d3474e300aa04ab87a08184208effb8
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-13 22:08:13 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -4,23 +4,20 @@
     <head>
         <meta chaeset="utf-8">
         <title>Admin Login Form</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="registration.css">
     </head>
     <body>
     <header>
-            <div class="wrapper">
-                <ul class="nav-area">
-                    <li><a href="input1.php">Home</a></li>
-                    <li><a href="index1.php">Sign Up</a></li>
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
-            </div>
-        </header>
-        
-        <div class="title"><h1>Admin Log in form</h1></div>
-        <div class="container">
-                <div class="formbox">
+        <p class="headding">Admin Login</p>
+        <nav>
+            <ul class="nav-links">
+               <li><a href="input1.php">Home</a></li>
+                <li><a href="about.php">About-us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+                <div class="formbox1">
                     <form action = "redirect_admin.php" method = "post">
                         <p>Username</p>
                         <input type="text" name="usn" placeholder="SSN">
@@ -30,7 +27,6 @@
                         <a href ="#">Forget Password</a>
                     </form>
                 </div>
-            </div>
         </div>
     </body>
 </html>
