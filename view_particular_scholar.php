5a3038f7b9e39ea0d44a89166a26e094a0e76627
padmashri5 <75256386+padmashri5@users.noreply.github.com>
2022-06-14 13:22:50 +0530

    Update view_particular_scholar.php

--- view_particular_scholar.php
+++ view_particular_scholar.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update</title>
+    <link href="registration.css" rel="stylesheet" type="text/css"/>
+    <title>particular</title>
 </head>
 <body>
+<header>
+        <p class="headding">particular Scholarship</p>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="input1.php">Home</a></li>
+                <!-- <li><a href="signin.php">Sign-in</a></li>
+                <li><a href="ligin.php">Login</a></li> -->
+                <li><a href="about.php">About-us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="formbody">
     <form action="redirect_view_particular_scholar.php" method="post">
     <label id="l1">Choose a Scholarship:</label>
     <select name="scholar" id="scholar">
@@ -30,66 +44,7 @@
       <br>
       <input type="submit" id="submitButton">
       </form>
+    </div>
+    
 </body>
 </html>
-<style>
-
-    body {
-    margin: 0;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #333;
-    background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url("https://cache.careers360.mobi/media/presets/720X480/colleges/social-media/media-gallery/3332/2018/8/9/SDM-College-of-Engineering-and-Technology-Dharwad-7.jpg");
-    background-size: cover;
-}
-
-ul {
-    padding: 0;
-    list-style-type: none;
-}
-input[type],select{
-    min-width: 2%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-    font-size: 15px;
-}
-li {
-    font-size: 25px;
-    width: 20em;
-    height: 2em;
-    color: rgb(255, 255, 255);
-    border-left: 0.08em solid;
-    position: relative;
-    margin-top: 0.8em;
-}
-
-li label{
-    position: relative;
-    height: 120%;
-    top: -10%;
-    box-sizing: border-box;
-    border: 0.08em solid;
-    background-color: hsl(0, 0%, 20%,0.9);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-family: sans-serif;
-    text-transform: capitalize;
-    transform: translateX(calc(-0.15em * 3 - 0.08em * 2));
-}
-#l1 {
-    color: aliceblue;
-}
-.submitButton{
-    padding-left: 50px;
-}
-.btn a {
-        font-size: medium;
-}
-</style>
