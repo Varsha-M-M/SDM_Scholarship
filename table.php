f888f12dc6c3fa6ef6679b56a4c9c9d5b674ac03
padmashri5 <75256386+padmashri5@users.noreply.github.com>
2022-06-14 15:27:45 +0530

    Add files via upload

---
+++ table.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="registration.css" rel="stylesheet" type="text/css"/>
+    <title>table</title>
+</head>
+<body>
+    <header>
+        <p class="headding">table</p>
+                <nav>
+                    <ul class="nav-links">
+                        <li><a href="input1.php">Home</a></li>
+                        <li><a href="signin.php">Sign-in</a></li>
+                        <li><a href="ligin.php">Login</a></li>
+                        <li><a href="about.php">About-us</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+    </header>
+
+    <table class="content-table">
+        <thead>
+            <tr>
+                <th>head1</th>
+                <th>head1</th>
+                <th>head1</th>
+                <th>head1</th>
+            </tr>
+            
+        </thead>
+        <tbody>
+            <tr>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+            </tr>
+            <tr>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+            </tr>
+            <tr>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+                <td>hi</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
