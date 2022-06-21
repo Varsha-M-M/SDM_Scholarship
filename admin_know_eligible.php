d9b742d0bc6b1562a3c382d1f1677dd06132e838
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-21 11:45:09 +0530

    Add files via upload

---
+++ admin_know_eligible.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta chaeset="utf-8">
+        <title>Login Form</title>
+        <link rel="stylesheet" href="registration.css">
+    </head>
+    <body>
+    <header>
+        <p class="headding">Student Eligible Scholarships</p>
+        <nav>
+            <ul class="nav-links">
+               <li><a href="input1.php">Home</a></li>
+                <li><a href="about.php">About-us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+      <div class="formbox1">
+          <form action = "result2.php" method = post>
+              <p>Username</p>
+              <input type="text" name="usn" placeholder="USN">
+              <input type="submit" name="" value="Submit">
+          </form>
+      </div>
+    </body>
+</html>
+
