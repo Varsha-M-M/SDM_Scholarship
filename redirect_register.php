0faef72fdda23d2dd917a04a1c749fc9dd490458
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-21 11:41:45 +0530

    Update redirect_register.php

--- redirect_register.php
+++ redirect_register.php
@@ -48,6 +48,102 @@ $_SESSION['id'] = $usn;
 
 
 if($result){
+
+
+
+
+    $conn = mysqli_connect("localhost","root","","projects") or die("connection refused");
+
+    $querry = "SELECT f.name,f.id
+    FROM fee_concession_for_disables f,student s
+    WHERE f.disbility_percentage<=s.disability_percentage AND s.usn = '$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM food_and_accomodation_obc f,student s
+    WHERE f.caste=s.caste AND f.income>=s.income AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM  fee_concession_others f,student s
+    WHERE f.income>=s.income AND f.caste=s.caste AND s.usn='$usn'   
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM food_and_accomodation_cat1 f,student s
+    WHERE f.income>=s.income AND f.prev_percentage<=s.prev_percentage AND f.caste = s.caste AND f.is_hostellite=s.is_hostellite AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM  post_matric_disable f,student s
+    WHERE f.disability_percentage  <= s.disability_percentage    AND s.usn='$usn' 
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM post_matric_labour f,student s
+    WHERE f.income>=s.income AND (f.occupation = s.father_occupation OR f.occupation = s.mother_occupation) AND s.usn = '$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM post_matric_sc f,student s
+    WHERE f.income>=s.income AND f.caste=s.caste AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM post_matric_st f,student s
+    WHERE f.income>=s.income AND f.caste=s.caste AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM saksham_disable f,student s
+    WHERE f.income>=s.income AND f.first_year=s.is_first AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM sdme_trust f,student s
+    WHERE f.income>=s.income AND f.prev_percentage<=s.prev_percentage AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM skdrdp f,student s
+    WHERE  f.prev_percentage<=s.prev_percentage AND f.is_member=s.is_member  AND s.usn='$usn'
+    
+    UNION
+    
+    SELECT f.name,f.id
+    FROM top_class_scheme_sc f,student s
+    WHERE  f.caste=s.caste  AND s.usn='$usn'
+    ";
+
+$result = mysqli_query($conn,$querry) or die("QUERRY UNSUCESSFULL");
+
+if(mysqli_num_rows($result) > 0){
+    while( $row = mysqli_fetch_assoc($result))
+    {
+        $id = $row['id'];
+
+       $query2 = "INSERT INTO student_scholarship(scholarship_id,usn) values('{$id}','{$usn}')";
+
+       $result2 = mysqli_query($conn,$query2) or die ("QUERY UNSUCCESS");
+    }
+}
+
+
+
+
+
+
+
     header("Location:http://localhost/project2/button.html");
 }
 
