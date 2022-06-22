32fda0b4544f9934a1918598f598c92f3c0866ba
Ashita B <103898786+Ashita-B@users.noreply.github.com>
2022-06-22 10:06:59 +0530

    Update send_otp_home.php

--- send_otp_home.php
+++ send_otp_home.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="registration.css" rel="stylesheet" type="text/css"/>
+    <title>Verification</title>
 </head>
 <body>
+<header>
+        <p class="headding">Enter OTP for Verification</p>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="input1.php">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="formbody">
     <form action='smtp/verify_otp.php' method='POST'>
         <label>Please enter OTP:</label>
         <input type="text" name="otp">
