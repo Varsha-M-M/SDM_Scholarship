8e566539465447ec5d9439886ce63aa20cab55a0
sonusonali07 <103899158+sonusonali07@users.noreply.github.com>
2022-06-22 12:48:21 +0530

    Add files via upload

--- result2.php
+++ result2.php
@@ -77,7 +77,7 @@ WHERE  f.caste=s.caste  AND s.usn='$usn'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="registration.css" rel="stylesheet" type="text/css"/>
+    <link href="registration1.css" rel="stylesheet" type="text/css"/>
     <title>Document</title>
 </head>
 <body>
